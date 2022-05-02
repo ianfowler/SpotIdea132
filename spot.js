7068b48b39707d7a09c2c9f40a16ddd114d85898
jaycoobs <cj.acosta@icloud.com>
2022-05-01 20:14:06 -0700

    Fixed score

--- spot.js
+++ spot.js
@@ -225,7 +225,7 @@
     // Mapping each track to the actualIdx will give the permutation
     // we want.
     let kd = kendall(tracks.map((t) => t.actualIdx));
-    return (kd / (tracks.length * (tracks.length - 1))) * 2;
+    return 1-(kd / (tracks.length * (tracks.length - 1))) * 2;
   }
 
   /**
