fe58f14d3c389c7c758ded06db942593436f79d9
jaycoobs <cj.acosta@icloud.com>
2022-05-01 19:15:40 -0700

    Implemented Kendall Distance algo

--- helpers.js
+++ helpers.js
@@ -9,3 +9,49 @@ function qs(selector) {
 function qsa(selector) {
   return document.querySelectorAll(selector);
 }
+
+/**
+ * For this spotify project, there should be another score function
+ * which formats the argument properly and normalizes the result.
+ *
+ * Algorithm to compute the Kendall distance between a permutation
+ * and (1 2 3 ... n) in nlogn time. Shoutout CS38.
+ *
+ * @param {array} p - a list of integers representing the permutation
+ */
+function kendall(p) {
+    // Aux algo
+    // Count the number of pairs i,j such that
+    // a[i] > b[j]
+    let countPairs = (a,b) => {
+        let c = 0;
+        let j = 0;
+        for (let i = 0; i < a.length; i++) {
+            while (j < b.length && a[i] > b[j]) {
+                j++;
+            }
+            c += j;
+        }
+        return c;
+    };
+
+    if (p.length < 2)
+        return 0;
+
+    let half = Math.ceil(p.length / 2);
+    let left = p.slice(0, half);
+    let right = p.slice(half, p.length);
+
+    let leftResult = kendall(left);
+    let rightResult = kendall(right);
+    let result = leftResult + rightResult + countPairs(left, right);
+
+    for (let i = 0; i < p.length; i++) {
+        if (i < half)
+            p[i] = left[i];
+        else
+            p[i] = right[i-half];
+    }
+
+    return result;
+}
