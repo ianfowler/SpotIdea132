f43a7ba87643191593ed5ede73bdf2e6857c3f56
Ian Fowler <ian@spidr.design>
2022-05-09 15:44:09 -0700

    push junk

--- spot.js
+++ spot.js
@@ -14,20 +14,6 @@
     // based on the client information...
     // (I had to set this up on the developer dashboard)
 
-    fetch("https://accounts.spotify.com/api/token", {
-      method: "POST",
-      headers: {
-        Authorization: "Basic " + btoa(CLIENT_ID + ":" + CLIENT_SECRET),
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: "grant_type=client_credentials",
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // Once we receive the access token we store it in a global...
-        accessToken = data.access_token;
-      });
-
     // Event handler for searching...
     let input = qs("input");
 
@@ -49,6 +35,54 @@
       showSection("search-view")
     );
   }
+
+  function getAccessToken() {
+    fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        Authorization: "Basic " + btoa(CLIENT_ID + ":" + CLIENT_SECRET),
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: "grant_type=client_credentials",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Once we receive the access token we store it in a global...
+        accessToken = data.access_token;
+      });
+  }
+
+  
+    /**
+   * Helper function to return the Response object if successful, otherwise
+   * throws an Error with an error status and corresponding text.
+   * @param {Response} response - response object to check for success/error
+   * @returns {object} - Response if status code is ok (200-level)
+   */
+     function checkStatus(response) {
+      if (!response.ok) {
+        throw Error("Error in request: " + response.statusText);
+      }
+      return response; // a Response object
+    }
+  
+    /**
+     * Displays an error message on the page, hiding any previous results.
+     * If errMsg is passed as a string, that string is used to customize an error message.
+     * Otherwise (the errMsg is an object or missing), a generic message is displayed.
+     * @param {String} errMsg - optional specific error message to display on page.
+     */
+    function handleError(errMsg) {
+      if (typeof errMsg === "string") {
+        id("message-area").textContent = errMsg;
+      } else {
+        // the err object was passed, don't want to show it on the page;
+        // instead use generic error message.
+        id("message-area").textContent =
+          "An error ocurred fetching the launch data";
+      }
+      id("message-area").classList.remove("hidden");
+    }
 
   /**
    * Make a request to the Spotify API for a given artist.
